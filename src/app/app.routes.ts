b6a9ab8548e9ce6725b4e76f92318f05ea652ed9
Quentin384 <zampieri.quentin38@gmail.com>
2025-05-16 14:34:38 +0200

    Adding home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'login', component: LoginComponent },
   { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
   { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent) },
 ];
-
